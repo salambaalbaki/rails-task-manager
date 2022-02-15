a007d7324e61812e98abe82ee275a05b24c6ed6e
Salam Baalbaki <salambaalbaki@gmail.com>
2022-02-15 15:53:08 +0400

    built a task manager using basic CRUD features

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # # See all tasks
+  # get '/tasks', to: 'tasks#index'
+
+  # # Create new task
+  # get '/tasks/new', to: 'tasks#new'
+  # post '/tasks', to: 'tasks#create'
+
+  # # Show details of one task
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # # Update existing task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # Delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
